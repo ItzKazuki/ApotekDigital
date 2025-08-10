ba628887d5127c85a0109e43499e66ceba70a968
ItzKazuki <79141593+ItzKazuki@users.noreply.github.com>
2025-08-10 22:22:16 +0700

    feat: add feature change profile image using cropperjs in kasir page

--- app/Http/Controllers/Kasir/ProfileController.php
+++ app/Http/Controllers/Kasir/ProfileController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Kasir;
 
 use App\Http\Controllers\Controller;
+use App\Traits\StoreBase64Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
-        public function index()
+    use StoreBase64Image;
+
+    public function index()
     {
         $user = Auth::user();
         return view('kasir.edit-profile', compact('user'));
@@ -21,11 +24,17 @@ class ProfileController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email,' . Auth::id(),
             'password' => 'nullable|string|min:8|confirmed',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'profile_img_base64' => 'nullable|string',
         ]);
 
+        $dataValidated['profile_image'] = $this->storeBase64Image('images/users', $request->profile_img_base64);
+
         $user = \App\Models\User::find(Auth::id());
+        
         foreach ($dataValidated as $key => $value) {
+            if ($key === 'profile_img_base64') {
+                continue; // skip profile_img_base64
+            }
             if ($key !== 'password' || !empty($value)) {
                 $user->$key = $value;
             }
@@ -34,6 +43,7 @@ class ProfileController extends Controller
         if (!empty($dataValidated['password'])) {
             $user->password = Hash::make($dataValidated['password']);
         }
+
         $user->save();
 
         return redirect()->route('kasir.profile')->with('success', 'Profile updated successfully.');
